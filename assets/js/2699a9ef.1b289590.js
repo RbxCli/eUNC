1fa1408b597d30c65b87c36d4335de7fb744f110
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2025-09-17 18:50:05 -0300

    Built and published by Moonwave

---
+++ assets/js/2699a9ef.1b289590.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[294],{9331:e=>{e.exports=JSON.parse('[{"value":"Functions","id":"functions","level":2},{"value":".WorldToScreen","id":"WorldToScreen","level":3}]')}}]);
--- assets/js/a3b1adb5.a2532033.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[900],{4410:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/RbxCli/eUNC/blob/dev","baseUrl":"/eUNC/","classOrder":[{"section":"Libraries","tag":"Library"}],"apiCategories":[]}')}}]);
